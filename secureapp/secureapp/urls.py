6a3597736087f9a4a40d1477bf107379565dbcaa
Amal Syatirah <amalsyatirah@MacBookAir.realtek>
2025-12-27 04:45:15 +0800

    remove dupe homepage redirect to login 2

--- secureapp/secureapp/urls.py
+++ secureapp/secureapp/urls.py
@@ -1,9 +1,11 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from core import views
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
